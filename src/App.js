999d026b7d3987816ee4cabfc27a266116fe9245
alienao <armin.orujyan@gmail.com>
2021-10-12 17:54:45 +0400

    build:login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import Login from './components/Login/Login';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import styles from './App.module.css';
 import QuestionList from './components/Login/QuestionList/QuestionList';
 
-function App() {
-  let jsonArr = [];
-  const userCreator = (user) => {
-    jsonArr.push(user);
-    console.log(jsonArr);
-  };
 
+function App() {
   return (
     <div>
       <Router>
         <Switch>
           <Route exact path="/">
-            {' '}
-            <Login userArr={jsonArr} userCreatorFunc={userCreator} />{' '}
+            <Login />
           </Route>
-          <Route exact path = "/QuestionList"> <QuestionList/></Route>
+          <Route exact path="/QuestionList">
+            {' '}
+            <QuestionList />
+          </Route>
         </Switch>
       </Router>
     </div>
